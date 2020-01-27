d719f39f0bd735485b111f21297826d502ad74ee
koshihara <satoshi.koshihara.yg@hitachi-solutions.com>
2020-01-27 16:15:26 +0900

    add manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,6 +1,6 @@
 package io.pivotal.pal.tracker;
 
-//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +13,14 @@ public class WelcomeController {
     }*/
 
 
-//    private final String message;
-//    public WelcomeController(@Value("${welcome.message}") String message) {
-//        this.message = message;
-//    }
+    private final String message;
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.message = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        //return "hello";
+        return this.message;
     }
 }
